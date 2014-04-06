abe4c21ffd69fc2caea4a778107f2ab9d7bfb028
Naftoreiclag <naftoreiclag@gmail.com>
2014-04-06 12:58:05 -0700

    Rocket mouse

--- source/PygameTest.py
+++ source/PygameTest.py
@@ -4,16 +4,24 @@ from pygame.locals import *
 print pygame.ver
 
 foo = "../resources/hail-damage.jpg"
+bar = "../resources/rocket-fly.png"
 
 pygame.init()
 screen = pygame.display.set_mode((640, 360), 0, 32)
 
 background = pygame.image.load(foo).convert()
+cursor = pygame.image.load(bar).convert_alpha()
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-            
+    
     screen.blit(background, (0, 0))
+    
+    x, y = pygame.mouse.get_pos()
+    
+    screen.blit(cursor, (x, y))
+    
+    pygame.display.update()
