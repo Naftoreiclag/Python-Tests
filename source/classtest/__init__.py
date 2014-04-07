484ebddf329fdb491e41f4d75962154daf1ec05d
Naftoreiclag <naftoreiclag@gmail.com>
2014-04-07 10:58:00 -0700

    objects!

---
+++ source/classtest/__init__.py
@@ -0,0 +1,14 @@
+
+import Pair, Triple
+
+print "Hello World"
+
+pair1 = Pair.Pair(1, 2)
+pair2 = Pair.Pair(3, 4)
+
+triple1 = Triple.Triple(1, 2, 3)
+triple2 = Triple.Triple(4, 5, 6)
+
+print pair1.x + pair2.x
+
+print triple1.x + triple2.y + triple1.z
